8bb3faa9609258e8a3ae354cb4c4c258813a307d
Scott McLamb <scott.mclamb@gmail.com>
2017-01-16 22:04:33 -0700

    Get search working.

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -28,8 +28,22 @@ module.exports = {
     });
 
     // Filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+      return searchText.length === 0 || todo.text.indexOf(searchText) > -1;
+
+    });
 
     // Sort Todos with non completed first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed) {
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
 
     return filteredTodos;
   }
