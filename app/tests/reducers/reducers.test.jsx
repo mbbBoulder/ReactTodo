9c0c2575e4f26b35a120dfce3f0405fbc8195743
Scott McLamb <scott.mclamb@gmail.com>
2017-01-19 21:42:01 -0700

    Add todos reducer.

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,6 +1,7 @@
 var expect = require('expect');
 var reducers = require('reducers');
 var df = require('deep-freeze-strict');
+var moment = require('moment');
 
 describe('Reducers', () => {
   describe('searchTextReducer', () => {
@@ -21,6 +22,44 @@ describe('Reducers', () => {
       };
       var res = reducers.showCompletedReducer(df(false), df(action));
       expect(res).toEqual(true);
+    });
+  });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todos', () => {
+      var todos = [
+        {
+          id: '3',
+          text: 'do this',
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        },
+        {
+          id: '4',
+          text: 'do that',
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        }
+      ];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: '4'
+      };
+      var res = reducers.todosReducer(df(todos), df(action));
+      expect(res[1].completed).toEqual(true);
+      expect(res[1].completedAt).toNotEqual(undefined);
     });
   });
 });
